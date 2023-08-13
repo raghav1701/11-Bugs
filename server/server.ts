074a53feb4d0b34a5bf373fe5563eb9c1483c290
Raghav Agrawal <raghav170101@gmail.com>
2023-08-14 02:14:08 +0530

    fix(): fixed typescript related errors

--- server/server.ts
+++ server/server.ts
@@ -7,12 +7,12 @@ import mongoose from "mongoose";
 import { fileURLToPath } from "url";
 
 // Routers
-import home from "./routes/home.js";
-import auth from "./routes/auth.js";
-import profile from "./routes/profile.js";
-import scrap from "./routes/scrap.js";
-import search from "./routes/search.js";
-import friends from "./routes/friends.js";
+import home from "../routes/home.js";
+import auth from "../routes/auth.js";
+import profile from "../routes/profile.js";
+import scrap from "../routes/scrap.js";
+import search from "../routes/search.js";
+import friends from "../routes/friends.js";
 
 // App
 const app = express();
@@ -24,8 +24,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 /* CONFIGURATIONS */
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 // Routes
 app.use("/", home);
@@ -40,8 +40,6 @@ const PORT = process.env.PORT || 6001;
 mongoose
     .connect(process.env.MONGO_URL, {
         dbName: "SaaS",
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
     })
     .then(() => {
         app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
